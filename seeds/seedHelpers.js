f09d1290cb12de0b4f16ef539a568430cfaaa31e
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-02-28 14:49:27 +0000

    added seed data to mongoose

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,25 @@
+module.exports.descriptors = [
+    'Silent',
+    'Elk',
+    'Diamond',
+    'Sea',
+    'Sky',
+    'Maple',
+    'Elm',
+    'Sky',
+    'Misty',
+    'Bullfrog'
+]
+
+module.exports.places = [
+    'Flats',
+    'Forest',
+    'Village',
+    'Canyon',
+    'Group Camp',
+    'Backcountry',
+    'Pond',
+    'River',
+    'Creek',
+    'Ghost Town'
+]
